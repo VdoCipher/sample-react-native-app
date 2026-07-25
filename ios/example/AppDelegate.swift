eac8b97f0a6b499ba07f5fdda2e687d81123f076
Mansi Sharma <mns.sharma17@gmail.com>
2026-07-25 16:13:16 +0530

    Updating vdocipher lib version 2.0.1

---
+++ ios/example/AppDelegate.swift
@@ -0,0 +1,32 @@
+import UIKit
+import React
+import React_RCTAppDelegate
+import ReactAppDependencyProvider
+
+@main
+class AppDelegate: RCTAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    self.moduleName = "example"
+    // Required on the New Architecture: provides the codegen'd module/component registry.
+    self.dependencyProvider = RCTAppDependencyProvider()
+    // You can add your custom initial props in the dictionary below.
+    self.initialProps = [:]
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func sourceURL(for bridge: RCTBridge) -> URL? {
+    self.bundleURL()
+  }
+
+  override func bundleURL() -> URL? {
+#if DEBUG
+    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+#else
+    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
+  }
+}
